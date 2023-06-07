5e1916de9f1cc8deb29e7df230d024859c76db11
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-07 19:06:04 +0900

    chore: js 작성

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const clickInputData = () => {
+  let todo = document.getElementById("todo_input").value;
+  let selc = document.getElementById("input_select").valuw;
+  console.log(todo, selc);
+};
