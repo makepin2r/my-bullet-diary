5c313ee4d22dd252197920b7a842f20777127f71
Kang-Gyeongwon <rosie02644@gmail.com>
2023-06-07 17:58:22 +0900

    chore: 다시

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
-
-from pymongo import MongoClient
-import certifi
-ca = certifi.where()
-client = MongoClient('mongodb+srv://sparta:test@cluster0.0mtn7sx.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)      # 주소 넣기
-db = client.dbsparta
-
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/inputData", methods=["POST"])
-def inputData_post():
-    bucket_receive = request.form['bucket_give']
-    doc = {
-        'bucket': bucket_receive
-    }
-    db.tododb.insert_one(doc)
-    return jsonify({'msg': '저장완료!'})
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    sample_receive = request.form['sample_give']
+    print(sample_receive)
+    return jsonify({'msg': 'POST 연결 완료!'})
     
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
